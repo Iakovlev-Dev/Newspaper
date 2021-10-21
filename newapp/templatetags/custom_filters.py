695ca410a3a8b485a88e7e5feff88ab1a8bd821d
Iakovlev-Dev <81575417+Iakovlev-Dev@users.noreply.github.com>
2021-10-21 20:54:57 +0300

    настроил фильтр

--- newapp/templatetags/custom_filters.py
+++ newapp/templatetags/custom_filters.py
@@ -7,9 +7,9 @@ register = template.Library()
 
 @register.filter(name='cenzor')
 def cenzor(text):
-    cenzor_text = text
-    for word in cenzor_text.split()[:]:
-        if word in CENZOR_VOCABULARY:
-            word = f'{word[0]}{re.sub("[a-zA-zа-яА-ЯёЁ]","*", word[1:-1])}{word[-2:]}'
+    cenzor_text = ''
+    for word in text.split()[:]:
+        if word.strip(',.\'"').lower() in CENZOR_VOCABULARY:
+           word = f'{word[0]}{re.sub("[a-zA-zа-яА-ЯёЁ]","*", word[1:-1])}{word[-1:]}'
         cenzor_text += f' {word}'
     return cenzor_text
